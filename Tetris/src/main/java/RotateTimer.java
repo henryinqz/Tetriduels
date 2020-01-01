f301cba01907ce3892e65b8597dbcd716689feaa
selwinjeremy <59151068+selwinjeremy@users.noreply.github.com>
2020-01-01 17:13:43 -0500

    Created Rotate timer class with instructions

---
+++ Tetris/src/main/java/RotateTimer.java
@@ -0,0 +1,26 @@
+public class RotateTimer implements Runnable{
+	
+	public void run(){
+		
+	}
+	
+	public void rotateBlock(){
+		
+	}
+	
+	public RotateTimer(){
+		
+	}
+}
+
+
+//Create a timer that activates whenever block collision occurs
+
+//timer.start should be activated in the "public static boolean checkCollision" method at the Controller.java class
+
+//Make the timer last only for 4 seconds --> This timer should allow tetris block to rotate 
+
+//Once the timer has been used for 4 seconds, the next block should drop under the screen
+
+//Once the timer has been used for 4 seconds, the block that is being rotated should be hard dropped 
+
