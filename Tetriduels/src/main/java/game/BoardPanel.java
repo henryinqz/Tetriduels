d0d59e1432537c2a82c8dac24cf50c4bc48544ee
zuhairsiddiqi <58523211+zuhairsiddiqi@users.noreply.github.com>
2020-01-15 23:07:21 -0500

    fixed hold gridlines

--- Tetriduels/src/main/java/game/BoardPanel.java
+++ Tetriduels/src/main/java/game/BoardPanel.java
@@ -149,7 +149,7 @@ public class BoardPanel extends JPanel {
         g2.setFont(Utility.loadFont("zorque"));
         Utility.setFontSize(g2,20);
         g2.drawString("HOLD", intHeldX, intHeldY - 28);
-        drawGridlines(intHeldX, intHeldY-20, intHeldX + (BLOCKSIZE * 4), intHeldY -20+ (BLOCKSIZE * 4), 4, 4, g2);
+        drawGridlines(intHeldX, intHeldY-28, intHeldX + (BLOCKSIZE * 4), intHeldY -28+ (BLOCKSIZE * 4), 4, 4, g2);
     }
 
     private void drawNextBlocks(Graphics2D g2) { // Draw next 3 blocks on sidebar
